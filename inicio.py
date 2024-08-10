36d4c3b9f49848d02daacfc904d9c5621f179bb7
buitragoramires <buitragoramires@gmail.com>
2024-08-10 17:42:56 +0000

    Nueva actualizacion 3 (pregunta el nombre y la edad)

--- inicio.py
+++ inicio.py
@@ -5,3 +5,6 @@ if edad >= 18:
   print("Puedes ingresar")
 else:
   print("No puedes ingresar, eres menor de edad")
+
+nombre = input("Introduce tu nombre: ")
+print("Tu edad es:", edad, "y tu nombre es:", nombre)
